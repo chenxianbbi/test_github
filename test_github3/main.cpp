489f73e6f18677c24dd5b8b338c8c510c6f143a3
chenxianbbi <chenxianbbi@163.com>
2020-04-15 13:32:25 +0800

    添加文件

---
+++ test_github3/main.cpp
@@ -0,0 +1,23 @@
+#include<opencv2\opencv.hpp>
+#include<iostream>
+#include<math.h>
+
+using namespace cv;
+using namespace std;
+
+Mat src, graySrc, dst;
+Mat binImage, morpImage;
+
+int main(int argc, char** argv) {
+	src = imread("F:/OneDrive - 汕头大学/Test Temp/opencv/part03/case04.jpg");
+	//判断是否成功读入
+	if (src.empty())
+	{
+		printf("could not load image...");
+		return -1;
+	}
+	imshow("inputImage", src);
+
+	waitKey(0);
+	return 0;
+}
